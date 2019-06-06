5a61f5609b51ece3291c5bde42a5e40d26874686
Jay Mody <jaykmody@gmail.com>
2019-06-06 15:26:37 -0400

    More changes

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -1,9 +1,11 @@
 ########## GERNERAL ##########
 alias salias='sublime ~/.bash_it/aliases/custom.aliases.bash'
 alias sbpf='sublime ~/.bash_profile'
-alias sbrc='sublime ~/.bash_profile'
+alias sbrc='sublime ~/.bashrc'
 
 alias subl='sublime'
+
+alias json='function __json() { jq -C . $* | less -R; unset -f __json; }; __json'
 ##############################
 
 
@@ -23,24 +25,24 @@ alias jupy='jupyter notebook'
 alias incognito='open -n "/Applications/Google Chrome.app" --args --incognito'
 
 # jaymsyndicate profile 10
-alias chr-syn='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 10"'
+alias syn-chrome='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 10"'
 alias youtube='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 10" --new-tab "youtube.com"'
 alias keep='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 10" --new-tab "https://keep.google.com/"'
 
 # @mcmaster profile 3
-alias chr-mac='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 3"'
+alias mac-chrome='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 3"'
 alias avenue='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 3" --new-tab "https://avenue.cllmcmaster.ca/d2l/home"'
 alias mosiac='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 3" --new-tab "https://epprd.mcmaster.ca/psc/prepprd/EMPLOYEE/EMPL/c/NUI_FRAMEWORK.PT_LANDINGPAGE.GBL?"'
 
 # jaymody10 profile 2
-alias chr-jay='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 2"'
+alias jay-chrome='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 2"'
 
 # jaykmody profile 9
-alias chr-jkm='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 9"'
+alias jkm-chrome='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 9"'
 alias github='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 9" --new-tab "https://github.com/"'
 
 # @magarveylab.ca profile 11
-alias chr-mag='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 11"'
+alias mag-chrome='open -n "/Applications/Google Chrome.app" --args --profile-directory="Profile 11"'
 
 ##############################
 
